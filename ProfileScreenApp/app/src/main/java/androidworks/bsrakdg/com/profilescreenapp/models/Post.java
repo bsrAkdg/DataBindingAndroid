2312968da6102357d9a429a61570ef83109ebcec
bsrAkdg <busraaakdag@gmail.com>
2018-11-17 20:58:02 +0300

    Post model added

---
+++ ProfileScreenApp/app/src/main/java/androidworks/bsrakdg/com/profilescreenapp/models/Post.java
@@ -0,0 +1,26 @@
+package androidworks.bsrakdg.com.profilescreenapp.models;
+
+import android.databinding.BindingAdapter;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class Post {
+
+    String imageUrl;
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @BindingAdapter("imageUrl")
+    public static void loadImage(ImageView imageView, String imageUrl){
+        Glide.with(imageView.getContext())
+                .load(imageUrl)
+                .into(imageView);
+    }
+}
